9b357ea443a4d8c4fcd78910054fbdb0cb865159
Ole Kristian Sunde <ole.sunde@gmail.com>
2013-11-30 20:32:37 +0100

    No vim in here

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
-ln -sf ~/.dotfiles/vimrc ~/.vimrc
-
-if [ ! -d ~/.vim ]; then
-	git clone https://github.com/scrooloose/vimfiles.git ~/.vim
-	cd ~/.vim && git submodule init && git submodule update
-else
-	cd ~/.vim && git submodule update
-fi
 
